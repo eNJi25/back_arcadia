da3e1e388151a9d6bb377a4391e09d214f4bc85d
eNJi25 <nfischer.contact@gmail.com>
2024-11-16 10:05:16 +0100

    Ajout du controller Habitat avec le crud

---
+++ src/Controller/HabitatController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Habitat;
+use App\Repository\HabitatRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('api/habitat', name: 'app_api_habitat_')]
+class HabitatController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private HabitatRepository $repository,
+        private UrlGeneratorInterface $urlGenerator,
+        private SerializerInterface $serializer,
+    ) {}
+
+    #[Route(name: 'new', methods: 'POST')]
+    public function new(Request $request): JsonResponse
+    {
+        $habitat = $this->serializer->deserialize($request->getContent(), Habitat::class, 'json');
+        $this->manager->persist($habitat);
+        $this->manager->flush();
+
+        $responseData = $this->serializer->serialize($habitat, 'json');
+        $location = $this->urlGenerator->generate(
+            'app_api_habitat_show',
+            ['id' => $habitat->getId()],
+            UrlGeneratorInterface::ABSOLUTE_URL,
+        );
+        return new JsonResponse($responseData, Response::HTTP_CREATED, ["location" => $location], true);
+    }
+
+    #[Route('/{id}', name: 'show', methods: 'GET')]
+    public function show(int $id): JsonResponse
+    {
+        $habitat = $this->repository->findOneBy(['id' => $id]);
+        if ($habitat) {
+            $responseData = $this->serializer->serialize($habitat, 'json');
+            return new JsonResponse($responseData, Response::HTTP_OK, [], true);
+        }
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/{id}', name: 'edit', methods: 'PUT')]
+    public function edit(int $id, Request $request): JsonResponse
+    {
+        $habitat = $this->repository->findOneBy(['id' => $id]);
+        if ($habitat) {
+            $habitat = $this->serializer->deserialize(
+                $request->getContent(),
+                Habitat::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $habitat]
+            );
+
+            $this->manager->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: 'DELETE')]
+    public function delete(int $id): JsonResponse
+    {
+        $habitat = $this->repository->findOneBy(['id' => $id]);
+        if ($habitat) {
+            $this->manager->remove($habitat);
+            $this->manager->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+}
